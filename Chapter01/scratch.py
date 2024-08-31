46540386272b03a437a636033280b6f168bb72e1
Qsbrown97 <qsbrown97@gmail.com>
2024-08-31 00:56:15 -0400

    Practice newline, taking input, and slicing

---
+++ Chapter01/scratch.py
@@ -0,0 +1,10 @@
+# A file to test out new ideas
+
+#newlines (\n) - moves to the next line
+print("Hello, \nWorld!")
+print("Hello, World!\n\nHello, World!")
+
+#get name input, print first letter of name
+name = input("What is your name? ")
+first_letter = name[0]
+print(first_letter)
