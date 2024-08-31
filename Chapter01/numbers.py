c1209f928442ca24fa15e2f286352f596c94fb7c
Qsbrown97 <qsbrown97@gmail.com>
2024-08-31 00:20:57 -0400

    Created chapter folder, created integers and float variables

---
+++ Chapter01/numbers.py
@@ -0,0 +1,10 @@
+# integers - whole numbers (negative or positive)
+apples = 4
+pears = 8
+attendees = 3042
+zip_code = 12345
+
+# floating point numbers - numbers with decimals
+price = 4.95
+gpa = 4.0
+pi = 3.14159
