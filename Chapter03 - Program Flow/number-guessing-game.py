7d7d1924255a4f2a349882e750e7d4886b162836
Qsbrown97 <qsbrown97@gmail.com>
2024-09-07 13:09:16 -0400

    Created function for number game

--- Chapter03 - Program Flow/number-guessing-game.py
+++ Chapter03 - Program Flow/number-guessing-game.py
@@ -1,12 +1,23 @@
 from random import seed
 from random import randint
 
-number = randint(1, 10)
-while number > 0:
-  print("Guess a number 1 through 10!")
-  user_guess = input("What is your guess?: ")
-  if int(user_guess) == number:
-    print("Whoa you're right, the number is " + str(number))
-    break
-  else:
-    print("Nope, try again!")
+def numberGame():
+  number = randint(10, 100)
+
+  while number > 0:
+
+    print("Guess a number 10 through 100!")
+    user_guess = input("What is your guess?: ")
+
+    if user_guess != "exit":
+      if int(user_guess) == number:
+        print("Whoa you're right, the number is " + str(number))
+      elif int(user_guess) != number:
+        print("Nope, try again!")
+        if number > int(user_guess):
+          print("Go higher!")
+        else:
+          print("Go lower!")
+    else: break
+
+numberGame()
